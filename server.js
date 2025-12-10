59e2f94f4bd96154fb4393a29ebc1d83a5924036
IDEA3C <idea3c@IDEA3CdeMacBook-Air.local>
2025-12-10 16:35:16 +0800

    添加 robots.txt 和 sitemap.xml，優化圖片載入策略和 server.js 路由

--- server.js
+++ server.js
@@ -7,7 +7,17 @@ const PORT = process.env.PORT || 3000;
 // 提供靜態文件服務
 app.use(express.static(__dirname));
 
-// 所有路由都返回 index.html（用於單頁應用）
+// 確保 robots.txt 和 sitemap.xml 可以被訪問
+app.get('/robots.txt', (req, res) => {
+  res.sendFile(path.join(__dirname, 'robots.txt'));
+});
+
+app.get('/sitemap.xml', (req, res) => {
+  res.set('Content-Type', 'text/xml');
+  res.sendFile(path.join(__dirname, 'sitemap.xml'));
+});
+
+// 所有其他路由都返回 index.html（用於單頁應用）
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
